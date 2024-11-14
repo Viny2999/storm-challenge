b9c18f0a30e58441ad6aacbf4e75d167a1cd37e8
Vinicius Menezes <viny2999@gmail.com>
2024-11-14 11:58:23 -0300

    feat: list refact

--- imdb-app/src/pages/MovieListPage.tsx
+++ imdb-app/src/pages/MovieListPage.tsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { Movie } from '../interfaces/Movie';
 import { Link } from 'react-router-dom';
 import { listMovies, setAuthToken } from '../service/http.service';
+import { Grid, Card, CardContent, CardMedia, Typography, Container, Alert } from '@mui/material';
 
 const MovieListPage: React.FC = () => {
   const [movies, setMovies] = useState<Movie[]>([]);
@@ -28,17 +29,43 @@ const MovieListPage: React.FC = () => {
   }, []);
 
   return (
-    <div>
-      <h2>Movies List</h2>
-      {error && <p style={{ color: 'red' }}>{error}</p>}
-      <ul>
-        {movies.map((movie, index) => (
-          <li key={index}>
-            <Link to={`/movies/${movie.id}`}>{movie.name} - {movie.director} - {movie.genre}</Link>
-          </li>
+    <Container>
+      <Typography variant="h4" component="h2" gutterBottom>
+        Movies List
+      </Typography>
+      {error && (
+        <Alert severity="error" style={{ marginBottom: '16px' }}>
+          {error}
+        </Alert>
+      )}
+      <Grid container spacing={3}>
+        {movies.map((movie) => (
+          <Grid item xs={12} sm={6} md={4} lg={3} key={movie.id}>
+            <Card>
+              <Link to={`/movies/${movie.id}`} style={{ textDecoration: 'none', color: 'inherit' }}>
+                <CardMedia
+                  component="img"
+                  height="200"
+                  image="https://fastly.picsum.photos/id/866/200/300.jpg?hmac=rcadCENKh4rD6MAp6V_ma-AyWv641M4iiOpe1RyFHeI"
+                  alt={movie.name}
+                />
+                <CardContent>
+                  <Typography variant="h6" component="div">
+                    {movie.name}
+                  </Typography>
+                  <Typography variant="body2" color="text.secondary">
+                    Director: {movie.director}
+                  </Typography>
+                  <Typography variant="body2" color="text.secondary">
+                    Genre: {movie.genre}
+                  </Typography>
+                </CardContent>
+              </Link>
+            </Card>
+          </Grid>
         ))}
-      </ul>
-    </div>
+      </Grid>
+    </Container>
   );
 };
 
